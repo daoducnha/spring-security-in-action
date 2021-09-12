da4e027f73c2f9deacbb0e43c5d23dbcbacb9fd2
daoducnha <daoducnha29493@gmail.com>
2021-09-12 18:56:02 +0700

    chap 13 user grant type is authorization code for authentication server

---
+++ chap_13_grant_type_code/src/main/java/com/example/chap_13_grant_type_code/config/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.example.chap_13_grant_type_code.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var userDetailsService = new InMemoryUserDetailsManager();
+
+        var u = User.withUsername("john")
+                .password("12345")
+                .authorities("read")
+                .build();
+
+        userDetailsService.createUser(u);
+        return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+}
---
+++ chap_13_grant_type_code/src/test/java/com/example/chap_13_grant_type_code/Chap13GrantTypeCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chap_13_grant_type_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap13GrantTypeCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
